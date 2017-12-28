2bedd8601f92cb6a89f6f14a5c76786d7c67dc72
farmaker47 <farmaker47@yahoo.cm>
2017-12-28 17:53:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.text.Spannable;
+import android.text.SpannableString;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    private String [] tabTitles = new String[] { "Με Όνομα", "Με Δραστική", "Με Barcode" };
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new TuesdayFragment();
+        } else if (position == 1){
+            return new MondayFragment();
+        } else if(position == 2) {
+            return new WednesdayFragment();
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        return tabTitles[position];
+
+       /* SpannableString sb = new SpannableString("   " + tabTitles[position]);
+        sb.setSpan(null, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
+*/
+    }
+}
